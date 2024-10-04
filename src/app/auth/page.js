6d15b070428963474168b2ee064075b9f5c3f926
Mahin <rakin99raj@gmail.com>
2024-10-04 15:38:08 +0600

    test-1

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -10,7 +10,7 @@ import SignUpForm from "@/components/SignUpForm";
 import ForgotPasswordForm from "@/components/ForgotPasswordForm";
 import ResetPasswordForm from "@/components/ResetPasswordForm";
 import google_logo from "../../../public/images/google-logo.png";
-import getZone from "@/lib/getZone";
+// import getZone from "@/lib/getZone";
 
 export default function AuthForm() {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -51,7 +51,7 @@ export default function AuthForm() {
 
   const fetchZones = async () => {
     try {
-      const result = await getZone();
+      const result = await fetch(`${domain}/api/zone/get-all-zone`);
       if (Array.isArray(result.data)) {
         setZones(result.data);
       } else {
